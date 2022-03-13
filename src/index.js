677420e1a30b3752712ead71d0db2c1c326ebe56
Arisa P <chilizhiki@gmail.com>
2022-03-13 05:23:50 +0000

    Adding streaming video

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 
+// Loads the (built-in) fs library so we can use the Node.js filesystem API.
+const fs = require("fs");
+
 const app = express();
 
 // listen on port 3000
 const port = 3000;
 
-// Creates a handler for the main HTTP route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Defines the HTTP route for streaming video.
+app.get("/video", (req, res) => {
+    const path = "../videos/SampleVideo_1280x720_1mb.mp4";
+
+    // Retrieves the video file size.
+    fs.stat(path, (err, stats) => {
+
+        // Handles errors
+        if (err) {
+            console.error("An error occurred");
+            res.sendStatus(500);
+            return;
+            }
+            
+        // Sends a response header to the web browser,
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+            });
+
+        // Streams the video to the web browser.
+        fs.createReadStream(path).pipe(res);
+    });
 });
 
 // Initiates the HTTP server
